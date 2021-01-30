b40a58255a7995bcfbc70a1511a5a4328858c120
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-01-30 18:04:18 -0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+import amino
+client = amino.Client()
+aminoId = input("Escribe un aminoId: ")
+com=client.search_community(aminoId)
+print(f"\n\nNombre de la comunidad: {com.name}\nAminoID: {com.aminoId}\ncomId: {com.comId}\n\n")
