cb31e9ee291d2135faf0011871019c3c20bcff87
VENOM-InstantDeath <mohathepro111@gmail.com>
2021-01-30 17:55:43 -0300

    Added main.py and install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: install.sh
+#
+#         USAGE: ./install.sh
+#
+#   DESCRIPTION: 
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: SH
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: DARTH VENOM (), 
+#  ORGANIZATION: HACKING-UTILS
+#       CREATED: 01/30/2021 05:51:59 PM
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+
+apt update -y
+whereis python &> /dev/null
+if [ $? != 0 ]; then apt install python -y; fi
+pip install amino.py
